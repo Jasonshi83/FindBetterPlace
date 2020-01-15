46d47d67870054829e15ec9fe118c5879c622015
Jason <shizong@outlook.com>
2020-01-15 20:39:30 +1300

    临时保存

--- retrieve.php
+++ retrieve.php
@@ -24,8 +24,13 @@
         $json = json_encode($data);
 //        echo $json;
 //        echo '{"address":"aaa","casedetail":"bbb","miseryIndex":"5","user_id":"1"}';
+//        echo '[{"address":"aaa","casedetail":"bbb","miseryIndex":"5","user_id":"1"},{"address":"aaa","casedetail":"bbb","miseryIndex":"5","user_id":"1"},{"address":"aaa","casedetail":"bbb","miseryIndex":"5","user_id":"1"},{"address":"webebreb","casedetail":"","miseryIndex":"5","user_id":"3"},{"address":"123121212","casedetail":"4562323","miseryIndex":"21","user_id":"23"},{"address":"123121212","casedetail":"4562323","miseryIndex":"21","user_id":"23"},{"address":"abc","casedetail":"ced","miseryIndex":"10","user_id":"1"},{"address":"abc","casedetail":"ced","miseryIndex":"10","user_id":"1"},{"address":"test","casedetail":"ttttttttt","miseryIndex":"12","user_id":"12"},{"address":"test","casedetail":"ttttttttt","miseryIndex":"122222","user_id":"12222"},{"address":"test","casedetail":"TT","miseryIndex":"7658","user_id":"222"},{"address":"city","casedetail":"TTTTTT","miseryIndex":"7658","user_id":"222"},{"address":"dfadf","casedetail":"dfdsf","miseryIndex":"11","user_id":"2"}]';
         echo $json;
-//        echo "hello world";
+//        var_dump($json);
+//        var_dump('{"address":"aaa","casedetail":"bbb","miseryIndex":"5","user_id":"1"}');
+//        var_dump($data);
+//        echo $data;
+
     } else {
         echo "Reading failed";
     }
